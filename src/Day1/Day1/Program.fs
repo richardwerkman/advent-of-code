7db20d98db625ec0631764ea8f59d8b7e8a9f92f
Richard Werkman <Richard.Werkman@elekta.com>
2021-12-02 17:15:59 +0100

    add day one

---
+++ src/Day1/Day1/Program.fs
@@ -0,0 +1,16 @@
+﻿open System.IO
+
+let list = File.ReadAllLines("input.txt") |> Seq.map System.Int32.Parse |> Seq.toList
+
+let answer1 = list |> Seq.pairwise
+                   |> Seq.filter(fun (a, b) -> a < b)
+                   |> Seq.length
+
+let answer2 = list |> Seq.windowed 3
+                   |> Seq.map(fun x -> Seq.sum x)
+                   |> Seq.pairwise
+                   |> Seq.filter(fun (a, b) -> a < b)
+                   |> Seq.length
+
+printfn "%O" answer1
+printfn "%O" answer2
