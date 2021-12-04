6527bf06ded5c6004d0c810e8692323d1ff0c483
Richard Werkman <Richard.Werkman@elekta.com>
2021-12-04 15:07:07 +0100

    Add day 4

---
+++ src/Day4/Day4/Bingo.fs
@@ -0,0 +1,4 @@
+﻿module Bingo
+
+//let play (numbers:int seq) (cards: int[][] seq) =
+    
---
+++ src/Day4/Day4/Program.fs
@@ -0,0 +1,9 @@
+﻿open System.IO
+open System
+
+let input = File.ReadAllLines("input.txt")
+
+let numbers = input[0].Split ',' |> Seq.map Convert.ToInt32 |> Seq.toList
+
+// For more information see https://aka.ms/fsharp-console-apps
+printfn "Hello from F#"
