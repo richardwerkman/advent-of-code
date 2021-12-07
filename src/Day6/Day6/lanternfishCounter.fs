5226f2422d2f6404c53c2edaa4699c35f85da305
Richard Werkman <Richard.Werkman@elekta.com>
2021-12-07 19:17:57 +0100

    Update day6

--- src/Day6/Day6/lanternfishCounter.fs
+++ src/Day6/Day6/lanternfishCounter.fs
@@ -1,19 +1,15 @@
 ﻿module lanternfishCounter
 
-let handleLanternfish (index:int) (states:int[]) = 
-    for i in 0 .. states.Length - 1 do
-        
+let run runs input =
+    [1 .. runs]
+    // Shift all items left (state.Tail) and append the original first element. Add first element to index 6.
+    |> Seq.fold (fun (state: int64 list) _ -> ((state.Tail |> List.mapi (fun i v -> if i = 6 then v + state[0] else v)) @ [state[0]])) input
+    |> Seq.sum
 
-let nextDay (counts:int[]) (day:int) :int[] = 
-    printfn "%O" $"After day {day}: {counts}"
-    counts |> Seq.fold (fun state index -> handleLanternfish index state) counts
-
-let countLanternfish (startingState:int List) (repeatCount:int) :bigint = 
+let countLanternfish (startingState:int List) (repeatCount:int) :int64 = 
     printfn "%O" $"Initial state: {startingState}"
-    let startMap = startingState |> Seq.countBy (fun x -> x)
-    let map = [| for _ in 0 .. 8 -> 0 |]
-    for (key, value) in startMap do
-       Array.set map key value
 
-    let result = {1..repeatCount} |> Seq.fold (fun state day -> nextDay state day) map
-    result.Length
+    let startingList = List.init 9 (fun _ -> 0L)
+    let counts = startingState |> Seq.fold (fun state x -> state |> List.updateAt x (state[x] + 1L)) startingList
+    
+    run repeatCount counts
--- src/Day6/ExampleTests/Tests.fs
+++ src/Day6/ExampleTests/Tests.fs
@@ -10,4 +10,4 @@ let ``Example input`` () =
     let input = File.ReadAllLines("input.txt")
     let startingState = input[0].Split ',' |> Seq.map Convert.ToInt32 |> Seq.toList
     let count = countLanternfish startingState 80
-    Assert.Equal(5934, count)
+    Assert.Equal(5934L, count)
